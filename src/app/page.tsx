905a430b36cea65b20068c1a2117e96cc73f6605
Alban Merino <alban.merino.me@gmail.com>
2024-08-07 00:50:29 +0200

    feat(login): suspense menu on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from "react"
 import { Menu } from "./components/Menu"
+import { MenuFallback } from "./components/MenuFallback"
 
 export default function Home() {
   return (
     <main className="flex size-full flex-col items-center">
-      <Menu />
+      <Suspense fallback={<MenuFallback />}>
+        <Menu />
+      </Suspense>
     </main>
   )
 }
